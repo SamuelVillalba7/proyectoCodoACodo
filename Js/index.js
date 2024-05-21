53b486406f4fb2164714964314d31eb748fa6713
melocotonoro <melisaoro@gmail.com>
2024-05-20 21:54:05 -0300

    Despliegue menú hamburguesa en tamaño tablet y mobile. Se elimina en Desktop

--- Js/index.js
+++ Js/index.js
@@ -1,11 +1,17 @@
 
+/* ACCORDION MENU FOOTER*/
 let accordeon=document.getElementById("foo-contain");
 
 accordeon.addEventListener("click",(e)=>{
-
 if(e.target.classList.contains("foo-title")){
-    console.log(e.target.parentElement)
     e.target.parentElement.classList.toggle("scale");
     e.target.children[1].classList.toggle("rotate")
 }
 })
+
+/*HAMBURGER MENU ACCORDION*/
+document.querySelector("#clickMe").addEventListener("click", function (evt) {
+    document.querySelector(".menu-section-accordeon").classList.toggle("hidden");
+});
+
+
